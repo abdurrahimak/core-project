9c20642abd40b83f78f68a7bdf7d9d67baab40ca
abdurrahimak <abdurrahimak@gmail.com>
2021-01-28 05:39:38 +0300

    add project

---
+++ Assets/Scripts/GameCont.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CoreProject.Pool;
+
+public class GameCont : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PoolerManager.Instance.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PoolerManager.Instance.GetPoolObjectForSeconds("EmptyGO", 3f);
+        }
+    }
+}
